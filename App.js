536fb793cf8fb6802439db37c311ad0b9f587a44
Mike2020x <62442141+Mike2020x@users.noreply.github.com>
2023-02-25 16:52:47 -0500

    initial commit
    Pequeña app echa accediendo a la WordPress API de la pagina css-tricks.com

---
+++ App.js
@@ -0,0 +1,16 @@
+import { Header } from "./app/components/Header.js";
+import { Loader } from "./app/components/Loader.js";
+import { Main } from "./app/components/Main.js";
+import { Router } from "./app/components/Router.js";
+import { InfiniteScroll } from "./app/helpers/infinite_scroll.js";
+
+export function App() {
+  const $root = document.getElementById("root");
+  $root.innerHTML = null;
+  $root.appendChild(Header());
+  $root.appendChild(Main());
+  $root.appendChild(Loader());
+
+  Router();
+  InfiniteScroll();
+}
